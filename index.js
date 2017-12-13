5020efdf0485e6fb77dd3eaa511f2571794560ba
Yohanan Assefa <nanahoy@gmail.com>
2017-12-13 12:08:07 -0700

    GET/POST/EDIT/DELETE for food are functional, need to check for bugs

--- index.js
+++ index.js
@@ -20,34 +20,10 @@ app.post('/api/v1/foods', FoodController.postFood);
 
 app.get('/api/v1/foods/:id', FoodController.getSingleFood);
 
-app.delete('/api/v1/foods/:id', FoodController.deleteAFood);
+app.delete('/api/v1/foods/:id', FoodController.deleteFood);
 
+app.put('/api/v1/foods/:id', FoodController.updateFood);
 
-// app.put('/api/v1/foods/:id', function (request, response) {
-//   var id = request.params.id;
-//   var name = request.body.name;
-//   var calories = request.body.calories;
-//
-//   if(!name && !calories) {
-//     return response.status(422).send({ error: "Food properties were not provided"})
-//   } else if (name && calories) {
-//     database.raw('UPDATE foods SET name=?, calories=? WHERE id=?', [name, calories, id])
-//     .then(function(data) {
-//       response.status(201).json(data.rows)
-//     })
-//   } else if (name) {
-//     database.raw('UPDATE foods SET name=? WHERE id=?', [name, id])
-//     .then(function(data) {
-//       response.status(201).json(data.rows)
-//     })
-//   } else if(calories) {
-//     database.raw('UPDATE foods SET calories=? WHERE id=?', [calories, id])
-//     .then(function(data) {
-//       response.status(201).json(data.rows)
-//     })
-//   }
-
-// })
 
 
 
