34f5c98474f1f990600c253eec8c638757f920b1
Yohanan Assefa <nanahoy@gmail.com>
2017-12-13 09:31:11 -0700

    Get and Post for food API

--- index.js
+++ index.js
@@ -11,3 +11,49 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', )
+
+
+app.get('/api/v1/foods', function(request, response) {
+  database.raw("SELECT id, name, calories FROM foods")
+  .then(function(data) {
+    if (data.rows == 0) { return response.sendStatus(404) }
+
+    response.json(data.rows);
+  })
+})
+
+app.get('/api/v1/foods/:id', function(request, response) {
+  var id = request.params.id;
+
+  database.raw("SELECT id, name, calories FROM foods WHERE id=?", [id])
+   .then(function(data) {
+     if (data.rows == 0) { return response.sendStatus(404) }
+
+     response.json(data.rows[0])
+   })
+})
+
+app.post('/api/v1/foods', function(request, response) {
+  var name = request.body.name;
+  var calories = request.body.calories;
+
+  if(!name && !calories) {
+    return response.status(422).send({ error: "Food properties were not provided"})
+  }
+
+  database.raw(
+    'INSERT INTO foods (name, calories) VALUES (?, ?) RETURNING id, name, calories',
+    [name, calories]
+  )
+  .then(function(data) {
+    response.status(201).json(data.rows)
+  })
+})
+
+
+
+
+
+app.listen(app.get('port'), function() {
+  console.log(`App is running on ${app.get('port')}.`)
+})
