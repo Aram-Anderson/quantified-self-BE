451cd3275e641a7c1f33833c2726bd6a2d56958c
Aram Anderson <aram.j.anderson@gmail.com>
2017-12-13 11:19:01 -0700

    Add foods to meals working

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ var app = express()
 var bodyParser = require('body-parser')
 const environment = process.env.NODE_ENV || 'development'
 const Meal = require('./lib/models/meal')
-const pry = require('pryjs')
 
 app.set('port', process.env.PORT || 3000)
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname+'/welcome.html'))
+})
 
 app.get('/api/v1/meals', (request, response) => {
   Meal.allMeals()
@@ -27,7 +30,7 @@ app.get('/api/v1/meals/:meal_id/foods', (request, response) => {
 })
 
 app.post('/api/v1/meals/:meal_id/foods/:id', (request, response) => {
-  Meal.addFoodToMeal(request.params.meal_id, request.params.id)
+  Meal.addFoodToMeal(request.params.id, request.params.meal_id)
   .then(function (data){
     if (data.rowCount === 0) { return response.sendStatus(404) }
     response.json(data.rows)
