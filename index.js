e3063a32b8bc2d066b8c3961807a794b81b3b1bd
Yohanan Assefa <nanahoy@gmail.com>
2017-12-13 11:31:44 -0700

    Deletes a food

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var bodyParser = require('body-parser')
 const environment = process.env.NODE_ENV || 'development'
 const configuration = require('./knexfile')[environment]
 const database = require('knex')(configuration)
+const FoodController = require('./lib/controllers/foods')
+
 
 app.set('port', process.env.PORT || 3000)
 
@@ -12,44 +14,40 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', )
 
+app.get('/api/v1/foods', FoodController.getAllFoods);
 
-app.get('/api/v1/foods', function(request, response) {
-  database.raw("SELECT id, name, calories FROM foods")
-  .then(function(data) {
-    if (data.rows == 0) { return response.sendStatus(404) }
+app.post('/api/v1/foods', FoodController.postFood);
 
-    response.json(data.rows);
-  })
-})
+app.get('/api/v1/foods/:id', FoodController.getSingleFood);
 
-app.get('/api/v1/foods/:id', function(request, response) {
-  var id = request.params.id;
+app.delete('/api/v1/foods/:id', FoodController.deleteAFood);
 
-  database.raw("SELECT id, name, calories FROM foods WHERE id=?", [id])
-   .then(function(data) {
-     if (data.rows == 0) { return response.sendStatus(404) }
 
-     response.json(data.rows[0])
-   })
-})
+// app.put('/api/v1/foods/:id', function (request, response) {
+//   var id = request.params.id;
+//   var name = request.body.name;
+//   var calories = request.body.calories;
+//
+//   if(!name && !calories) {
+//     return response.status(422).send({ error: "Food properties were not provided"})
+//   } else if (name && calories) {
+//     database.raw('UPDATE foods SET name=?, calories=? WHERE id=?', [name, calories, id])
+//     .then(function(data) {
+//       response.status(201).json(data.rows)
+//     })
+//   } else if (name) {
+//     database.raw('UPDATE foods SET name=? WHERE id=?', [name, id])
+//     .then(function(data) {
+//       response.status(201).json(data.rows)
+//     })
+//   } else if(calories) {
+//     database.raw('UPDATE foods SET calories=? WHERE id=?', [calories, id])
+//     .then(function(data) {
+//       response.status(201).json(data.rows)
+//     })
+//   }
 
-app.post('/api/v1/foods', function(request, response) {
-  var name = request.body.name;
-  var calories = request.body.calories;
-
-  if(!name && !calories) {
-    return response.status(422).send({ error: "Food properties were not provided"})
-  }
-
-  database.raw(
-    'INSERT INTO foods (name, calories) VALUES (?, ?) RETURNING id, name, calories',
-    [name, calories]
-  )
-  .then(function(data) {
-    response.status(201).json(data.rows)
-  })
-})
-
+// })
 
 
 
