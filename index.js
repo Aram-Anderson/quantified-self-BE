0bee9fd268300a7d0bbd0700810564ba43996a0e
Aram Anderson <aram.j.anderson@gmail.com>
2017-12-13 15:21:06 -0700

    Pull Yohanan's branch

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ app.set('port', process.env.PORT || 3000)
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 app.get('/', function (request, response) {
   response.sendFile(path.join(__dirname +'/index.html'));
